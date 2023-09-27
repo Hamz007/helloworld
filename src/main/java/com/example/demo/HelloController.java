8a924067713bc7cb6b81a5197cea14f0ea78e564
Hamza <LE2029384@crcmail.net>
2023-09-27 09:50:49 -0400

    Added exitButton into the program

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+@FXML
+private Button exitButton;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void setExitButton() {
+        exitButton.setOnAction(event -> {
+            System.exit(0);
+        });
     }
 }
